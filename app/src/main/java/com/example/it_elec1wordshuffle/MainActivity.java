4fddf3c43e8ed643bcf30c06e836f1f6f4b043b7
grw99 <granthreggwolf@gmail.com>
2021-09-18 15:56:10 +0800

    1

--- app/src/main/java/com/example/it_elec1wordshuffle/MainActivity.java
+++ app/src/main/java/com/example/it_elec1wordshuffle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.it_elec1wordshuffle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = null;
+        btn[0] = findViewById();
+        btn[1] = findViewById();
+        btn[2] = findViewById();
+        btn[3] = findViewById();
+        btn[4] = findViewById();
+        btn[5] = findViewById();
     }
+
+    //Declations
+    Button[] btn;
 }
